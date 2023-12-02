8cf3fadbc1a3524bd2c1ac367313655da7b04bd4
Tommy Bidne <tbidne@protonmail.com>
2023-12-02 23:31:17 +1300

    Test PR

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,5 +4,25 @@ module Lib (foo) where
 
 import System.OsPath.Encoding (EncodingException)
 
+-- This throws deprecation warnings:
+--import System.OsPath.Encoding.Internal (EncodingException)
+--
+-- src/Lib.hs:8:41: error: [GHC-68441] [-Wdeprecations, -Werror=deprecations]
+--     In the use of type constructor or class ‘EncodingException’
+--     (imported from System.OsPath.Encoding.Internal):
+--     Deprecated: "Use System.OsString.Encoding.Internal from os-string >= 2.0.0 package instead. This module will be removed in filepath >= 1.5."
+--   |
+-- 8 | import System.OsPath.Encoding.Internal (EncodingException)
+--   |                                         ^^^^^^^^^^^^^^^^^
+
+-- src/Lib.hs:10:15: error: [GHC-68441] [-Wdeprecations, -Werror=deprecations]
+--     In the use of type constructor or class ‘EncodingException’
+--     (imported from System.OsPath.Encoding.Internal):
+--     Deprecated: "Use System.OsString.Encoding.Internal from os-string >= 2.0.0 package instead. This module will be removed in filepath >= 1.5."
+--    |
+-- 10 | foo :: Either EncodingException ()
+--    |               ^^^^^^^^^^^^^^^^^
+-- Error: cabal: Failed to build osstr-deprecated-0.1.
+
 foo :: Either EncodingException ()
 foo = Right ()
